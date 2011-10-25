eae89cbd1cb6c0bc316396d9ff38842d6dd3617b
EtienneDion <dionetienne@gmail.com>
2011-10-25 17:33:10 -0400

    Plan de travail

---
+++ plan.js
@@ -0,0 +1,37 @@
+// JavaScript Document
+
+
+simon
+
+simon.buttons
+
+simon.buttons[2].shape
+
+simon.playback = function() {
+}
+
+simon.playButton = function(button)
+
+simon.playNextButton
+
+simon.listenToUser 
+
+simon.userPlaying = true;
+simon.onEndPlayback
+simon.onEndUserPlay
+simon.onUserNextButton
+simon.onUserFail
+simon.onUserSuccess
+simon.currentLevel // 3
+simon.levels
+
+/*
+
+*/
+function Level(button) {
+	button = // Object reference to the button
+	coord = // Position where it has been fixed
+	size = // Size at which it was fixed
+	getPositions = function
+	seed(gameSeed + coord + size) = function //
+}
